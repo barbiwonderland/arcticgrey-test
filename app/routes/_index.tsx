36b9ef15e7a2358b4c7d4886fda5bcf026fa98ab
barbiwonderland <baarbarabottazzi@gmail.com>
2025-04-09 20:50:40 -0300

    marquee

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,6 +6,7 @@ import type {
   FeaturedCollectionFragment,
   RecommendedProductsQuery,
 } from 'storefrontapi.generated';
+import Marquee from '~/components/Marquee';
 
 export const meta: MetaFunction = () => {
   return [{title: 'Hydrogen | Home'}];
@@ -79,24 +80,28 @@ function FeaturedCollection({
       to={`/collections/${collection.handle}`}
     >
       {image && (
-        <div className="featured-collection-image relative">
+        <div className="featured-collection-image relative !h-screen w-screen ">
           {/* <Image data={image} sizes="100vw" /> */}
           <video
             width="100%"
             autoPlay
             muted
             loop
+            className="!h-screen object-cover"
             src="https://cdn.shopify.com/videos/c/o/v/00895dae9f1948d08c6d42b6cf20e338.mp4"
             typeof="video/mp4"
           ></video>
         </div>
       )}
-      <div className="w-[854px] !h-[264px]  absolute top-[550px] left-[40px] flex flex-wrap content-between flex-row">
-        <h1 className="font-main text-white font-semibold w-full !text-[70px] !m-0 !leading-none">
+      <div className="md:w-[854px] md:!h-[264px]  md:absolute md:top-[550px] md:left-[40px] flex flex-wrap md:content-between flex-row sm:top-[322px] sm:left-[16px] sm:w-[95%] sm:content-around">
+        <h1 className="font-main text-white font-semibold w-full md:!text-[70px] !m-0 !leading-none sm:!text-xs">
           Great things never came from comfort zones.
         </h1>
-        <div className="bg-white h-[50px] w-[160px] rounded-lg flex justify-center items-center ">Shop Now</div>
+        <div className="bg-white h-[50px] w-[160px] rounded-lg flex justify-center items-center ">
+          Shop Now
+        </div>
       </div>
+      <Marquee />
     </Link>
   );
 }
