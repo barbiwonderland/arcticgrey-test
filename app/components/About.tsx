42febaff2b0440a0286f34bfc044c1da232fd36f
barbiwonderland <baarbarabottazzi@gmail.com>
2025-04-12 01:40:56 -0300

    about section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,39 +1,65 @@
 
+import Consultation from '../assets/icons/about/consultation (1) 1.png';
+import Clipbord from '../assets/icons/about/clipboard 1.png'
+import Like from '../assets/icons/about/like 1.png';
+import Leaves from '../assets/icons/about/leaves (1) 1.png';
+
+const features = [
+  {
+    icon: Like,
+    title: 'We Make It Easy',
+    subtitle:
+      'Personalized Solutions & Guidance Mean You Get Just What You Need Nothing More',
+  },
+  {
+    icon: Leaves,
+    title: 'Clean & Effective',
+    subtitle:
+      'Proven Ingredients, not Artificial, Crafted By Experts For Optimal Effectiveness',
+  },
+  {
+    icon: Clipbord,
+    title: 'Your Free Dietitian',
+    subtitle:
+      'Every Gainful Subscriber Gets Free, 1:1 Access Their Own Registered Dietitian.',
+  },
+  {
+    icon: Consultation,
+    title: 'Made For You',
+    subtitle:
+      'Performance Is Personal. Personalized & Customizable Products For Your Needs, Body & Goals',
+  },
+];
+
 function About() {
   return (
-    <div className="main-about flex flex-col h-screen w-screen main-font ">
-    <div className="text w-[419px] h-32">
-   <div className="heading text-[16px]">
-    🧐 Why Health & Fitness
-   </div>
-   <div className="title text-[40px] font-medium">
-Clean Supplements - Made For You
-   </div>
-
+    <div className="main-about flex flex-col w-screen main-font h-[514px] justify-center ">
+      <div className="text w-[400px] h-32">
+        <div className="heading text-[16px]">🧐 Why Health & Fitness</div>
+        <div className="title text-[40px] font-medium">
+          Clean Supplements - Made For You
         </div>
-  <div className="benefits mt-12 flex flex-row w-full gap-8">
-    <div className="container flex flex-col w-[356px] h-auto">
-        <div className="icon bg-black w-12 h-12 rounded-full mb-6">s</div>
-        <div className="title text-[18px] font-medium mb-4">We Make It Easy</div>
-        <div className="subtitle text-[16px]">Personalized Solutions & Guidance Mean You Get Just What You Need Nothing More</div>
-
+      </div>
+      <div className="benefits mt-12 flex flex-row w-full gap-8">
+        {features.map((item, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                className="container flex flex-col w-[350px] h-auto"
+              >
+                <div className="icon bg-black w-12 h-12 rounded-full mb-6 text-white flex items-center justify-center"><img src={item.icon} alt={item.title} /></div>
+                <div className="title text-[18px] font-medium <BiLike /><GiThreeLeaves /><TbClipboardText />mb-4">
+                  {item.title}
+                </div>
+                <div className="subtitle text-[16px] text-gray-700">{item.subtitle}</div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
-     <div className="container flex flex-col w-[356px] h-auto">
-        <div className="icon bg-black w-12 h-12 rounded-full mb-6">s</div>
-        <div className="title text-[18px] font-medium mb-4">We Make It Easy</div>
-        <div className="subtitle text-[16px]">Personalized Solutions & Guidance Mean You Get Just What You Need Nothing More</div>
-
-    </div>
-     <div className="container flex flex-col w-[356px] h-auto">
-        <div className="icon bg-black w-12 h-12 rounded-full mb-6">s</div>
-        <div className="title text-[18px] font-medium mb-4">We Make It Easy</div>
-        <div className="subtitle text-[16px]">Personalized Solutions & Guidance Mean You Get Just What You Need Nothing More</div>
-
-    </div>
-    </div>
-    </div>
-  
-  )
+  );
 }
 
-export default About
+export default About;
